f1241b1c73a7851ff49b32ae3345a48de55c9d88
Jorge <jcn.0954@gmail.com>
2021-01-04 22:16:01 -0300

    encadenamiento

--- python/OOP/user/main.py
+++ python/OOP/user/main.py
@@ -8,36 +8,27 @@ class usuario:
 
     def make_deposit(self, monto):
         self.cta_bancaria+=monto
+        return self
 
     def make_withdrawal(self, cantidad):
         self.cta_bancaria-=cantidad
+        return self
 
     def display_user_balance(self):
         print("Usuario ",self.nombre, "Monto ",self.cta_bancaria)
+        return self
 
     def transfer_money (self, other_user,cantidad):
         self.make_withdrawal(cantidad)
         other_user.make_deposit(cantidad)
+        return self
 
     
 jorge=usuario("Jorge","j@mail.com")
 gustavo=usuario("Gustavo","g@mail.com")
 oscar=usuario("Oscar", "o@mail.com")
-jorge.make_deposit(30)
-jorge.make_deposit(10)
-jorge.make_deposit(20)
-jorge.make_withdrawal(10)
-jorge.display_user_balance()
-gustavo.make_deposit(30)
-gustavo.make_deposit(30)
-gustavo.make_withdrawal(20)
-gustavo.make_withdrawal(10)
-gustavo.display_user_balance()
-oscar.make_deposit(50)
-oscar.make_withdrawal(10)
-oscar.make_withdrawal(15)
-oscar.make_withdrawal(10)
-oscar.display_user_balance()
-jorge.transfer_money(oscar,30)
-jorge.display_user_balance()
+jorge.make_deposit(30).make_deposit(10).make_deposit(20).make_withdrawal(10).display_user_balance()
+gustavo.make_deposit(30).make_deposit(30).make_withdrawal(20).make_withdrawal(10).display_user_balance()
+oscar.make_deposit(50).make_withdrawal(10).make_withdrawal(15).make_withdrawal(10).display_user_balance()
+jorge.transfer_money(oscar,30).display_user_balance()
 oscar.display_user_balance()
